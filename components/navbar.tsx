f152e5b96d1bde2261b413507b807f8dc852440a
JKrishnaD <jayak5063@gmail.com>
2024-08-20 23:27:24 +0530

    settingUp

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+
+export const Navbar = () => {
+  return (
+    <div className="text-white flex flex-row justify-between p-4 px-20">
+      <div className="text-xl font-bold font-mono">ThoughtSpace</div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
