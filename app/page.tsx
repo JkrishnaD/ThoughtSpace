9902aa0ac38ef34262cd79eb092258f147afa896
JKrishnaD <jayak5063@gmail.com>
2024-08-21 21:26:51 +0530

    dataBaseAdded

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import { LandingPage } from "@/components/landing";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
       <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
       <div className="flex justify-center items-center h-full">
-      <LandingPage/>
+        <div>
+          <h1>Thought Space</h1>
+          <div className="flex flex-col justify-center items-center text-xl">
+            <div>"Capture Your Thoughts And Share Your Voice"</div>
+            <Button>
+              <Link href="/home">Explore More</Link>
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/thoughtbox.tsx
+++ components/thoughtbox.tsx
@@ -3,15 +3,15 @@ import { Button } from "@nextui-org/button";
 export const ThoughtBox = () => {
   return (
     <div>
-      <div className="flex items-center justify-center flex-col lg:flex-row space-x-4 font-mono">
+      <div className="flex items-center justify-center flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 font-mono">
         <Input
           className="backdrop-blur-lg bg-transparent h-fit placeholder:text-white text-white w-[300px] lg:w-[600px] p-4"
-          placeholder="Share Your Thought"
+          placeholder="Share Your Thought....."
         />
         <Button
           size="lg"
           variant="bordered"
-          className="bg-inherit text-white border hover:bg-white hover:text-black rounded-md p-3.5 transition "
+          className="bg-inherit text-white border hover:bg-white hover:text-black rounded-md p-4 transition "
         >
           share
         </Button>
