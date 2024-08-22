f6d702a23e57aa8ea14a1d5cb5e78c93fe2f1fea
JKrishnaD <jayak5063@gmail.com>
2024-08-22 19:09:05 +0530

    HomePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <SessionProvider>
     <html lang="en">
       <body>
         <main className={inter.className}>{children}</main>
       </body>
     </html>
+    </SessionProvider>
   );
 }
