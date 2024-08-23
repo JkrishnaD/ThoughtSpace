012035d12db3c1965ea198fa555beb62b91f3741
JKrishnaD <jayak5063@gmail.com>
2024-08-24 01:43:03 +0530

    HomePage

--- app/mythoughts/page.tsx
+++ app/mythoughts/page.tsx
@@ -1,8 +1,11 @@
+import { Navbar } from "@/components/navbar";
 
 const MyThoughtsPage = ()=>{
     return (
-        <div>
-        My Thoughts
+        <div className="min-h-screen inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+            <div>
+                <Navbar/>
+            </div>
         </div>
     )
 }
